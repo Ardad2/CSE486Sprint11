94c0950165f40424d9f44ce8bd2f1c5a8daf12eb
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-11-11 15:56:15 -0700

    added calculation for statistic for each behavior

--- components/BehaviorStatItem.js
+++ components/BehaviorStatItem.js
@@ -6,9 +6,31 @@ function BehaviorStatItem(props) {
 
     const navigation = useNavigation();
 
+    const userList = useSelector( (state) => state.users.users);
+    const currUser = userList.filter(user => user.username == authToken.email);
+    const behaviorList = currUser[0].behaviorLogs;
+  
+
     function pressHandler() {
       props.onPress(props.behaviorName, props.id, props.behaviorID, props.date
       )
+    }
+
+    function calculateAverage() 
+    {
+      var totalMeasurment = 0;
+      var completedMeasurment = 0;
+
+      for (var i = 0; i < behaviorList.length; i++)
+      {
+        if (behaviorList[i].behaviorID == props.behaviorID)
+        {
+          totalMeasurment+=behaviorList[i].goalCount;
+          completedMeasurment+=behaviorList[i].count;
+        }
+      }
+
+      return (completedMeasurment)/(totalMeasurment);
     }
     
 
@@ -20,7 +42,7 @@ function BehaviorStatItem(props) {
         style={({pressed}) => pressed && styles.pressedItem}
         > 
         <View> 
-        <Text style={styles.behaviorText}>{props.name} {props.goalCount} </Text>
+        <Text style={styles.behaviorText}>{props.name} {calculatedAverage()} </Text>
         </View>
         </Pressable>
         </View>
